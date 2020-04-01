9c49d58be43d69271c7ca81dc097bf4b11899fb5
lepompiste <julienrobin71@outlook.fr>
2020-04-01 15:32:11 +0200

    Ctrl C handling to close the database

--- server/server.go
+++ server/server.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -37,6 +39,17 @@ func successResponse(w http.ResponseWriter, r interface{}) {
 	w.Write(bytes)
 }
 
+func setupCloseHandler(s *server) {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("Server shutdown, closing database")
+		s.db.Close()
+		os.Exit(0)
+	}()
+}
+
 // InitServer starts the webserver, initializing api, fileserver and installation directory
 // dir = root of file server, dbp = database path, port = port of the server
 func InitServer(dir, dbp, port string) {
@@ -46,6 +59,8 @@ func InitServer(dir, dbp, port string) {
 	SERVER.dbpath = dbp
 
 	SERVER.initInstall()
+
+	setupCloseHandler(&SERVER)
 
 	err := os.Chdir(SERVER.dirpath)
 	if err != nil {
